9c81b3264275ffa0487ea0214a2f7e2f1e631a2c
Manar Alsofiani <63370381+ManarAlsofiani@users.noreply.github.com>
2020-06-26 18:25:24 +0300

    Delete delete.php

--- delete.php
+++
@@ -1,6 +0,0 @@
-<?php
- include_once("add.php");
-$select = "Delete from ROBOT_MAP where id='".$_GET['del_id']."'";
-$query= mysqli_query($conn1,$select)or die($select);
-header("location:add.php");
- ?>
